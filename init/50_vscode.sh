dcdbcf9c15327b4fcbd44e6cc10d82c86bedc975
m4gie <steeven.regnault@epitech.eu>
2021-02-17 16:56:16 +0100

    vscode create user directory

--- init/50_vscode.sh
+++ init/50_vscode.sh
@@ -1,6 +1,6 @@
 
 # Link vscode configs
-mkdir -p ~/.dotfiles/.vscode/
+mkdir -p ~/.dotfiles/.vscode/User/
 ln -sf ~/.dotfiles/.vscode/keybindings.json ~/.config/Code/User/keybindings.json 
 ln -sf ~/.dotfiles/.vscode/settings.json ~/.config/Code/User/settings.json 
 ln -sf ~/.dotfiles/.vscode/snippets ~/.config/Code/User 
